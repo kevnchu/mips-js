f28413a470b0c86145a52597e6317fc22e5c74c1
Kevin Chu <kevnchu@gmail.com>
2016-06-06 22:11:15 -0400

    completely changed scanner

--- src/scanner.js
+++ src/scanner.js
@@ -1,82 +1,114 @@
+// Assembler Syntax
+
+// Program includes .data and .text
+// Comments begin with #.  Rest of line is ignored.
+// Identifier names are sequence of letters, numbers, underbars (_) and dots (.).
+// Labels are declared by putting them at beginning of line followed by colon.  Use labels for variables and code locations.
+// Instruction format: op field followed by one or more operands: addi $t0, $t0, 1
+// Operands may be literal values or registers.
+// Register is hardware primitive, can stored 32-bit value: $s0
+// Numbers are base 10 by default.  0x prefix indicates hexadecimal.
+// Strings are enclosed in quotes.  May include \n=newline or \t=tab.  Used for prompts.
+
 const instructions = require('./instructions')
 
 const isInstruction = (str) => {
     return instructions.hasOwnProperty(str.toUpperCase());
 };
 
-// trim whitespace, strip comments
-const sanitize = (str) => {
-    const commentIndex = str.indexOf('#');
-    if (commentIndex >= 0) {
-        str = str.substr(0, commentIndex);
-    }
-    return str.trim();
-};
-
-const preprocess = function (lines) {
-    const dataRe = /^\.data$/;
-    const textRe = /^\.text$/;
-    const dataIndex = lines.findIndex((x) => dataRe.test(x));
-    const textIndex = lines.findIndex((x) => textRe.test(x));
-    const cutIndex = Math.max(dataIndex, textIndex);
-    const first = lines.slice(0, cutIndex);
-    const second = lines.slice(cutIndex);
+const charIter = (str) => {
+    let index = 0;
+    const generator = function* () { 
+        for (let c of str) {
+            ++index;
+            yield c;
+        }
+    };
+    
+    charGenerator = generator(str);
+    
     return {
-        //data: dataIndex === cutIndex ? second : first,
-        //text: dataIndex === cutIndex ? first : second
-        data: dataIndex,
-        text: textIndex
+        getChar: () => charGenerator.next().value,
+        peek: () => str[index],
+        currentChar: () =>  str[index - 1]
     };
 };
 
-const scan = function* (program) {
-    // token classes
-    const label = /^[A-z_][A-z0-9]*\s*:\s*$/;
-    const directive = /^\.[A-z]+/;
-    const numericalConstant = /^(\d+|0x[0-9a-f]+)$/i;
-    const register = /^\$/;
-    const identifier = /^[0-9A-z]+$/;
-    const address = /[0-9]*\([\$0-9A-z]+\)$/;
+// token classes
+// const label = /^[A-z_][A-z0-9]*\s*:\s*$/;
+// const directive = /^\.[A-z]+/;
+// const numericalConstant = /^(\d+|0x[0-9a-f]+)$/i;
+// const string = /^".*"$/;
+// const register = /^\$/;
+// const identifier = /^[0-9A-z]+$/;
+// const address = /[0-9]*\([\$0-9A-z]+\)$/;
 
-    let lines = program.split('\n').map((str) =>
-                                          sanitize(str));
-    lines = preprocess(lines);
-    // TODO split up scanning by data and instructions
-    return;
-    for (let text of lines) {
-	if (!text) {
-	    continue;
-	}
-        const words = text.split(/[,\s]+/);
-        for (let word of words) {
-            let type;
-            if (label.test(word)) {
-                type = 'label';
-            } else if (directive.test(word)) {
-                type = 'directive';
-            } else if (isInstruction(word)) {
+// regexps
+const alpha = /[A-z]/;
+const numeric = /[0-9]/;
+const alphaNumeric = /[A-z0-9]/;
+const whitespace = /\s/;
+
+// generator function returns an iterable that yields tokens
+const scan = function* (program) {
+    const {currentChar, getChar, peek} = charIter(program);
+    let curr = getChar();
+    const scanWhile = (regex) => {
+        let lexeme = '';
+        do {
+            lexeme += curr;
+            curr = getChar();
+        } while (regex.test(curr));
+        return lexeme;
+    };
+    
+    while (curr) {
+        if (whitespace.test(curr)) {
+            curr = getChar();
+            continue;
+        }
+        let type;
+        let lexeme = '';
+        if (alpha.test(curr)) {
+            type = 'identifier';
+            lexeme = scanWhile(alphaNumeric);
+            if (isInstruction(lexeme)) {
                 type = 'instruction';
-            } else if (numericalConstant.test(word)) {
-                type = 'number';
-            } else if (register.test(word)) {
-		type = 'register';
-	    } else if (identifier.test(word)) {
-                type = 'identifier';
-            } else if (address.test(word)) {
-                type = 'address';
             }
-            if (!type) {
-                throw {
-                    name: 'Scanner error',
-                    message: 'Invalid token ' + word
-                };
+        } else if (numeric.test(curr)) {
+            type = 'int';
+            lexeme = scanWhile(numeric);
+            lexeme = parseInt(lexeme);
+        } else if (curr === '.') {
+            type = 'directive';
+            lexeme = scanWhile(alphaNumeric);
+        } else if (curr === '$') {
+            type = 'register';
+            lexeme = scanWhile(alphaNumeric);
+        } else if (curr === '"') {
+            type = 'string';
+            do {
+                lexeme += curr;
+                curr = getChar();
+            } while (curr && curr !== '"');
+            lexeme += curr;
+            curr = getChar();
+        } else if (curr === '#') {
+            // ignore comment
+            while (curr && curr !== '\n') {
+                curr = getChar();
             }
-            yield {
-                value: word,
-                type
-            };
+            continue;
+        } else {
+            type = 'ascii';
+            lexeme = curr;
+            curr = getChar();
+        }
+        yield {
+            value: lexeme,
+            type: type
         }
     }
 };
 
-module.exports = { scan };
+module.exports = { scan, charIter };
